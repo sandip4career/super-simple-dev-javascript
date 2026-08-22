2a0e116db48734d6598d94fe746dec61e8ff0641
sandip4career <sandiipyadav87@gmail.com>
2026-08-22 21:10:30 +0530

    Make the cart quantity interactive

--- javascript-amazon/scripts/amazon.js
+++ javascript-amazon/scripts/amazon.js
@@ -143,5 +143,11 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
                 });
             }
 
+            let cartQuantity = 0;
+cart.forEach((item) => {
+    cartQuantity+=item.quantity;
+});
+
+document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
 });
