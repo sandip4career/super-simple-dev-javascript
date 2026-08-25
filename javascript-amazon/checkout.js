c0f33b68812976086e9ce854c3a6c9649b0e3714
sandip4career <sandiipyadav87@gmail.com>
2026-08-25 22:11:17 +0530

    Delete products from html

--- javascript-amazon/checkout.js
+++ javascript-amazon/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
 
 
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -50,7 +50,7 @@ cart.forEach((cartItem) => {
                             Update
                         </span>
 
-                        <span class="delete-quantity-link link-primary js-delete-link data-product-id=${matchingProduct.id}">
+                        <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                             Delete
                         </span>
                     </div>
@@ -124,7 +124,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click',() => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     })
 });
 
