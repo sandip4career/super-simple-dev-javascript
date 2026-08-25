10367da905acdc0905610242dcee110cde96c732
sandip4career <sandiipyadav87@gmail.com>
2026-08-25 21:51:47 +0530

    Delete product from cart when clicking delete

--- javascript-amazon/checkout.js
+++ javascript-amazon/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart,removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -50,7 +50,7 @@ cart.forEach((cartItem) => {
                             Update
                         </span>
 
-                        <span class="delete-quantity-link link-primary">
+                        <span class="delete-quantity-link link-primary js-delete-link data-product-id=${matchingProduct.id}">
                             Delete
                         </span>
                     </div>
@@ -119,5 +119,13 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click',() => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        console.log(productId);
+    })
+});
 
 
